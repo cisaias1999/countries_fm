b48f027674b5eb326da824fcdebf426be0a4c37b
cisaias <cisaias9904@gmail.com>
2023-07-23 18:55:42 -0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Forms from "@/Components/Form/Forms";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-[20px] md:p-[50px] w-full ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-[20px] md:p-[50px] w-full bg-[#FAFAFA] ">
       <Forms />
     </main>
   );
